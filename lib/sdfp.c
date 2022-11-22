39698866c87352b4046826a2daad5e5d9e2f385c
evaitl <evaitl@gmail.com>
2022-11-22 11:49:56 -0500

    Initial stats and control

--- lib/sdfp.c
+++ lib/sdfp.c
@@ -10,12 +10,92 @@
 #include <linux/ptrace.h>
 #include <linux/sched/task_stack.h>
 #include <linux/export.h>
+#include <linux/debugfs.h>
 
 static bool sdfp_kill_doublefetch; 
 static DECLARE_BITMAP(sdfp_ignored_calls, NR_syscalls) = { 0 };
 static DECLARE_BITMAP(sdfp_multiread_reported, NR_syscalls) = { 0 };
+static unsigned num_multis[NR_syscalls];
+static unsigned long num_bytes[NR_syscalls];
+struct dentry *sdfp_dir;
 
+/**
+   For stats, all writes will reset the stats data.
 
+   For read:
+
+   4\t2\t8\t12\n = 30
+   nr   enabled   num_multis   num_bytes
+
+   30 bytes per line 
+ */
+static ssize_t stats_write(struct file *file, const char __user *ubuf,
+                           size_t count, loff_t *ppos){
+        memset(num_multis,0,sizeof(num_multis));
+        memset(num_bytes,0,sizeof(num_bytes));
+        bitmap_zero(sdfp_multiread_reported,NR_syscalls);
+        return count;
+}
+#define STATS_LEN 30
+static ssize_t stats_read(struct file *file, char __user *ubuf,size_t count, loff_t *ppos){
+        char buf[STATS_LEN+1];
+        int nr=*ppos/STATS_LEN;
+        ssize_t res=0;
+        while (nr < NR_syscalls &&  count > STATS_LEN){
+                bool enabled=test_bit(nr,sdfp_ignored_calls);
+                memset(buf,0,sizeof(buf));
+                snprintf(buf,sizeof(buf),"%4u\t%2u\t%8u\t%12lu\n",
+                         nr, enabled, num_multis[nr], num_bytes[nr]);
+                if (copy_to_user(ubuf, buf, STATS_LEN)){
+                        break;
+                }
+                ubuf += STATS_LEN;
+                *ppos+=STATS_LEN;
+                nr=*ppos/STATS_LEN;
+                res+=STATS_LEN;
+                count -=STATS_LEN;
+        }
+        return res;
+}
+const static struct file_operations stat_fops={
+        .read=stats_read,
+        .write=stats_write,
+};
+
+static ssize_t disable_write(struct file *file, const char __user *ubuf,
+                             size_t count, loff_t *ppos){
+        unsigned nr=0;
+        int res=kstrtouint_from_user(ubuf,count,10,&nr);
+        if (res){
+                return res;
+        }
+        if(nr>=NR_syscalls){
+                return -ERANGE;
+        }
+        set_bit(nr,sdfp_ignored_calls);
+        *ppos+=count;
+        return count;
+}
+const static struct file_operations disable_fops={
+        .write=disable_write,
+};
+static ssize_t enable_write(struct file *file, const char __user *ubuf,
+                             size_t count, loff_t *ppos){
+        unsigned nr=0;
+        int res=kstrtouint_from_user(ubuf,count,10,&nr);
+        if (res){
+                return res;
+        }
+        if(nr>=NR_syscalls){
+                return -ERANGE;
+        }
+        clear_bit(nr,sdfp_ignored_calls);
+        *ppos+=count;
+        return count;        
+}
+const static struct file_operations enable_fops={
+        .write=enable_write,
+};
 /**
  * Set up the bitfields or other data for sdfp.
  */
@@ -24,6 +104,14 @@ static int __init sdfp_init(void)
 	set_bit(__NR_write, sdfp_ignored_calls);
 	set_bit(__NR_execve, sdfp_ignored_calls);
 	set_bit(__NR_futex, sdfp_ignored_calls);
+        sdfp_dir=debugfs_create_dir("sdfp",0);
+        if(sdfp_dir==(struct dentry *)ERR_PTR){
+                return 1;
+        }
+        debugfs_create_bool("df_kill",0600,sdfp_dir,&sdfp_kill_doublefetch);
+        debugfs_create_file("stats",0600,sdfp_dir,0,&stat_fops);
+        debugfs_create_file("enable",0200,sdfp_dir,0,&enable_fops);
+        debugfs_create_file("disable",0200,sdfp_dir,0,&disable_fops);
 	return 0;
 }
 module_init(sdfp_init);
@@ -120,6 +208,7 @@ static bool overlap_check(uint8_t *to, struct sdfp_node *cn,
 	if ((start > cn->end) || (end < cn->start))
 		return false;
 	// Some kind of multi-fetch happened.
+        num_multis[nr]+=1;
 	if (!test_and_set_bit(nr, sdfp_multiread_reported))
 		printk(KERN_ALERT "SDFP multiread seen in pid %d syscall %d",
 		       current->pid, nr);
@@ -161,6 +250,7 @@ void sdfp_check(void *to, const void __user *from,
 	const uintptr_t end = start + n;
 	if (test_bit(nr, sdfp_ignored_calls))
 		return;
+        num_bytes[nr]+=n;
 	while (cn && !merged) {
 		// Look for overlaps and merges. Check bytes if overlaps.
 		merged = overlap_check(to, cn, start, end);
